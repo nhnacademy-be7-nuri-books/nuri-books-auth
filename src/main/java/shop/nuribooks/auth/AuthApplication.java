a3b884a7cb210b2641c508d2596e02668400c234
Yongtaek Lee <itaekit@gmail.com>
2024-10-22 09:50:05 +0900

    init

---
+++ src/main/java/shop/nuribooks/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package shop.nuribooks.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
