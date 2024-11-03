5c430c4e1f36ace47a00abfaa6a47aaf3d9710d3
Yongtaek Lee <itaekit@gmail.com>
2024-11-03 18:41:26 +0900

    refactor: Exception Handling
    - GlobalExceptionHandler 추가
    - Customized Exception 추가
    - authenticationEntryPoint() 내부 예외 처리 로직 수정

--- src/main/java/shop/nuribooks/auth/common/feign/MemberFeignClient.java
+++ src/main/java/shop/nuribooks/auth/common/feign/MemberFeignClient.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import shop.nuribooks.auth.dto.MemberResponse;
 
 @FeignClient(name = "gateway")
+// @FeignClient(name = "memberFeignClient", url = "http://localhost:8083")
 public interface MemberFeignClient {
 	@GetMapping("/api/member/{username}")
 	ResponseEntity<MemberResponse> findByUsername(@PathVariable String username);
