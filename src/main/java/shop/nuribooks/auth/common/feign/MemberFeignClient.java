76be9964f6cc0774e109f3f26728f5442860b571
Yongtaek Lee <143694269+itaekit@users.noreply.github.com>
2024-11-26 19:18:43 +0900

    feat: 비회원 조회 구현 (#61)

--- src/main/java/shop/nuribooks/auth/common/feign/MemberFeignClient.java
+++ src/main/java/shop/nuribooks/auth/common/feign/MemberFeignClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import shop.nuribooks.auth.dto.MemberResponse;
+import shop.nuribooks.auth.dto.NonMemberResponse;
 
 @FeignClient(name = "memberFeignClient", url = "http://localhost:8083")
 public interface MemberFeignClient {
@@ -18,4 +19,7 @@ public interface MemberFeignClient {
 
 	@PutMapping("/api/members/{username}/login-time")
 	ResponseEntity<Void> informLogin(@PathVariable String username);
+
+	@GetMapping("/api/members/customers/{email}")
+	ResponseEntity<NonMemberResponse> findNonMemberByEmail(@PathVariable String email);
 }
